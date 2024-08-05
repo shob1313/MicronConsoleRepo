2ca993d4569aaf454a7c46c491d5fc9ff30307c8
krr <ab8@gmail.com>
2024-08-05 13:51:24 +0530

    First change made in Program.cs

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -1,2 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+//Console.WriteLine("Hello, World!");
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("First Commit");
+
+    }
+}
